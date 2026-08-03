db96a7470b345c750ccf517ff8cb7df83f9f90a7
Qinis <qinisogazu588@gmail.com>
2026-08-03 04:02:11 +0200

    fix: replace data-router-only ScrollRestoration with ScrollToTop to fix blank screen

--- src/components/storefront/StorefrontLayout.tsx
+++ src/components/storefront/StorefrontLayout.tsx
@@ -1,17 +1,26 @@
-import { Outlet, ScrollRestoration } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 
 import { Header } from "@/components/storefront/Header";
 import { Footer } from "@/components/storefront/Footer";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 export function StorefrontLayout() {
   return (
     <div className="flex min-h-screen flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
-      <ScrollRestoration />
     </div>
   );
 }
